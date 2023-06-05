7adc4b07f63a1d8df034fc7422c68118cc470714
Shivam010011 <135306939+Shivam010011@users.noreply.github.com>
2023-06-05 22:07:37 +0530

    Assignment Module 1

---
+++ module1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main() {
+	
+	printf("My Name is Shivam Singh.\n");
+	printf("I was born on 17th October 2009.\n");
+	printf("I am 13 years old.\n");
+	printf("My Address is 13 Air Flats, Nr. Oxygen Park, Paldi, Ahmedabad, 380024.\n");
+}
