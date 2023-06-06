952247b5c8a8def74934ec0a6bba25a97a68001b
Shivam010011 <135306939+Shivam010011@users.noreply.github.com>
2023-06-06 17:06:28 +0530

    Assignment Module 2

---
+++ Module2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main() {
+	
+	int a;
+	
+	printf("Enter your Number: ");
+	scanf("%d",&a);
+	
+	printf("The Square of your number is %d\n",a*a);
+	printf("The Cube of your number is %d\n",a*a*a);
+}
+
